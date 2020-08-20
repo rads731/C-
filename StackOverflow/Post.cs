cafa9c57fee43bbbf079e7122bcdedaac3e530a2
Radhika Singh <30948939+rads731@users.noreply.github.com>
2020-08-20 19:30:23 +0530

    Add files via upload

---
+++ StackOverflow/Post.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Post {
+        public string Title;
+        public string Description;
+        public DateTime Date_Time;
+        private int TotalVote;
+
+       public Post(string ti,string des,DateTime dateTime) {
+            this.Title = ti;
+            this.Description = des;
+            this.Date_Time = dateTime;
+            //this.TotalVote = 0;
+        }
+
+       
+        public int UpVote() {
+            TotalVote += 1;
+            return TotalVote;
+        }
+        public int DownVote() {
+            TotalVote -= 1;
+            return TotalVote;
+        }
+
+        public void Display() {
+            Console.WriteLine("{0}  {1}  {2}  {3}  ",Title,Description,Date_Time,TotalVote);
+        }
+    }
+}
---
+++ StackOverflow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Post p1 = new Post("Post1", "MY First Post", DateTime.Now);
+            Post p2 = new Post("Post2", "My Second Post", DateTime.Now);
+            for (int i = 0; i < 10; i++) {
+                p1.UpVote();
+            }
+            p1.Display();
+            p2.UpVote();
+            p2.Display();
+        }
+    }
+}
