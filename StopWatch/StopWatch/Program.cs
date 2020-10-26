a48e97faecd598920afbedd437ce2b9bad357a0c
Radhika Singh <30948939+rads731@users.noreply.github.com>
2020-10-27 00:34:16 +0530

    Add files via upload

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StopWatch st1 = new StopWatch();
+            //StopWatch st2 = new StopWatch();
+            Console.WriteLine("{0}",st1.Start());
+            for(int i=0;i<200;i++)
+            Thread.Sleep(500);
+            Console.WriteLine("{0}", st1.Stop());
+        }
+    }
+}
---
+++ StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class StopWatch
+    {
+        private DateTime t1=DateTime.Now;
+
+        public DateTime Start() { return t1; }
+        public DateTime Stop() { return t1; }
+
+    }
+}
